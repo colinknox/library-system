adcf26baf84bcb2fd3884bb81aac793292543e77
ColinKnox <colinwknox@gmail.com>
2026-02-05 10:55:38 -0500

    completed LibraryItem class

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ class LibraryItem:
     def __init__(self, title, item_id):
         self.title = title
         self.item_id = item_id
+
+    def get_description(self):
+        return f"{self.title} (ID: {self.item_id})"
+    
+    def can_borrow(self):
+        return True
+
+book = LibraryItem("Neuromancer", "N123")
+
+print(book.can_borrow())
