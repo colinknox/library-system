2e572ee1d0a774c85ff44ea247fb58ee9807ffe1
colinknox <colinwknox@gmail.com>
2026-02-06 06:41:33 -0500

    added checkout method to Book class

--- main.py
+++ main.py
@@ -13,10 +13,24 @@ class Book(LibraryItem):
     def __init__(self, title, item_id, author):
         super().__init__(title, item_id)
         self.author = author
-        self.__is_checked_out = False
+        self._is_checked_out = False
+
+    def checkout(self):
+        if self._is_checked_out == True:
+            raise Exception("Book is already checked out")
+        else:
+            self._is_checked_out = True
+
 
 
 
 book = LibraryItem("Neuromancer", "N123")
+hobbit = Book("The Hobbit", "H321", "J. R. R. Tolkien")
 
-print(book.can_borrow())
+print(hobbit._is_checked_out)
+print(hobbit.checkout())
+print(hobbit._is_checked_out)
+
+
+
+
